e1992462b4544d08c0f4ee043c901e5fa492c37a
learrsan <gaslas@hotmail.com>
2015-04-07 20:01:46 +0000

    20.01

--- GestionListasMVCWeb/Controllers/HomeController.cs
+++ GestionListasMVCWeb/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security;
 using System.Web;
 using System.Web.Mvc;
 using GestionListasMVCWeb.Models;
@@ -13,6 +15,12 @@ namespace GestionListasMVCWeb.Controllers
         [SharePointContextFilter]
         public ActionResult Index()
         {
+
+            Session["SPHostUrl"] = Request.QueryString["SPHostUrl"];
+            Session["SPLanguage"] = Request.QueryString["SPLanguage"];
+            Session["SPClientTag"] = Request.QueryString["SPClientTag"];
+            Session["SPProductNumber"] = Request.QueryString["SPProductNumber"];
+
             User spUser = null;
 
             var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
@@ -42,7 +50,36 @@ namespace GestionListasMVCWeb.Controllers
         [HttpPost]
         public ActionResult Alta(Alumno alumno)
         {
-            return View();
+           
+            using (var context = new ClientContext("https://letias.sharepoint.com"))
+            {
+                var pwd = new SecureString();
+                foreach (var c in "Cursill0".ToCharArray())
+                {
+                    pwd.AppendChar(c);
+                }
+
+                context.Credentials = new SharePointOnlineCredentials("‎learrsan@letias.onmicrosoft.com", pwd);
+               
+                List list = context.Web.Lists.GetByTitle("Alumnos");
+                context.Load(list);
+                var creacion = new ListItemCreationInformation();
+                var item = list.AddItem(creacion);
+                item["Title"] = alumno.Nombre;
+                item["Apellido"] = alumno.Apellido;
+                item["Edad"] = alumno.Edad;
+                item["Nota"] = alumno.Nota;
+
+                item.Update();
+
+                context.ExecuteQuery();
+            }
+            String redireccion = String.Format("/?SPHostUrl={0}&SPLanguage={1}&SPClientTag{2}&" +
+                                               "SPProductNumber={3}", Session["SPHostUrl"],
+                Session["SPLanguage"], Session["SPClientTag"],
+                Session["SPProductNumber"]);
+
+            return RedirectToAction("Index","Home");
         }
     }
 }
